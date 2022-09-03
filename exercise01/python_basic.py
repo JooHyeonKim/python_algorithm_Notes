230a225da6d05b386ac29b4e299d76a9b966fef4
JooHyeonKim <subin_282@naver.com>
2022-09-03 06:07:11 -0700

    2022.09.03

--- exercise01/python_basic.py
+++ exercise01/python_basic.py
@@ -209,21 +209,21 @@ print(data)
 # input() : 한 줄의 문자열을 입력받는 함수
 # map()   : 리스트의 모든 원소에 각각 특정한 함수를 적용할 때 사용
 # 예시 : 공백을 기준으로 구분된 데이터를 입력 받을 때는 다음과 같이 사용합니다.
-#   list(map(int, input().split()))
+   list(map(int, input().split()))
 
 # 예시 : 공백을 기준으로 구분된 데이터의 개수가 많지 않다면, 단순히 다음과 같이 사용합니다
-#   a,b,c = map(int, input().split())
+   a,b,c = map(int, input().split())
 
 #데이터의 개수 입력
-#n = int(input())
+n = int(input())
 
 #각 데이터를 공백을 기준으로 구분하여 입력 split()
-#data = list(map(int, input().split())) #많이 사용되는 형태!!
-#data.sort(reverse=True)
+data = list(map(int, input().split())) #많이 사용되는 형태!!
+data.sort(reverse=True)
 print(data)
 
-#a,b,c = map(int, input().split())
-#print(a,b,c)
+a,b,c = map(int, input().split())
+print(a,b,c)
 
 #빠르게 입력 받기
 # 사용자로부터 입력을 최대한 빠르게 받아야 하는 경우
