30e92ee7dfaa6c2e106938d6740abb9b3fbc0c8b
JooHyeonKim <subin_282@naver.com>
2022-09-09 20:58:56 -0700

    chusuck

--- exercise01/라이브러리/library.py
+++ exercise01/라이브러리/library.py
@@ -65,3 +65,33 @@ from itertools import product
 data = ['A','B','C']
 result = list(product(data, repeat=2))
 print(result)
+#[('A', 'A'), ('A', 'B'), ('A', 'C'), ('B', 'A'), ('B', 'B'), ('B', 'C'), ('C', 'A'), ('C', 'B'), ('C', 'C')]
+
+#중복조합
+from itertools import combinations_with_replacement
+data = ['A','B','C'] #데이터 준비
+result = list(combinations_with_replacement(data,2)) #2개를 뽑는 모든 조합 구하기(중복 허용)
+print(result)
+# [('A', 'A'), ('A', 'B'), ('A', 'C'), ('B', 'B'), ('B', 'C'), ('C', 'C')]
+
+#Counter **유용
+# 파이썬 collections 라이브러리의 Counter는 등장 횟수를 세는 기능 제공
+# 리스트와 같은 반복 가능한(iterable) 객체가 주어졌을 때 내부의 원소가 몇 번씩 등장했는지를 알려줍니다.
+
+from collections import Counter
+
+counter = Counter(['red','blue','red', 'green','blue','blue'])
+print(counter['red']) #red가 등장한 횟수 출력 : 2
+print(counter['blue']) #blue 횟수 출력 : 3
+print(dict(counter)) #사전 자료형으로 반환
+
+#최대공약수와 최소 공배수
+import math
+
+#최소 공배수(LCM)를 구하는 함수
+def lcm(a,b):
+    return a*b //math.gcd(a,b) #a*b를 최대공약수로 나눈 값 = 최소공배수
+a = 21
+b = 14
+print(math.gcd(a,b)) #최대공약수(gcd) 계산
+print(lcm(a,b))
