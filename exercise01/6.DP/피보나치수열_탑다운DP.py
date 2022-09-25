5907f8bfabd780a54628b4351cc0ea9834c23bbb
JooHyeonKim <subin_282@naver.com>
2022-09-24 20:18:57 -0700

    cobucammangham

---
+++ exercise01/6.DP/피보나치수열_탑다운DP.py
@@ -0,0 +1,16 @@
+#한 번 계산된 결과를 메모이제이션
+
+d = [0]*100
+
+def fibbo(x):
+    if x == 1 or x == 2:
+        return 1
+
+    if d[x] != 0:
+        return d[x]
+
+    else:
+        d[x] = fibbo(x-1) + fibbo(x-2)
+        return d[x]
+
+print(fibbo(99))
